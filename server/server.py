d56bce8668a5f29318743ff37bdefd1e0e8ca21c
Cooper Pellaton <c@cepp.at>
2016-11-12 08:36:13 -0500

    Store the credentials in a global variable and then switch to httplib2.

--- server/server.py
+++ server/server.py
@@ -1,5 +1,6 @@
 import requests
 import logging
+import httplib2
 import json
 import tempfile
 import sys
@@ -13,6 +14,10 @@ from flask import Flask, jsonify, redirect, url_for, request, Response
 from flask_login import *
 from pymongo import MongoClient
 from bson import json_util
+
+# Single user auth credentials
+credentials = None
+http = httplib2.Http()
 
 # Flask configuration
 app = Flask(__name__)
@@ -54,83 +59,34 @@ def get_real_token():
                                scope='https://www.googleapis.com/auth/youtube',
                                redirect_uri='http://li507-39.members.linode.com/api/oauth2callback')
     code = request.args.get('code')
+    global credentials
     credentials = flow.step2_exchange(code)
     logging.info("Here is the code: ")
-    # url_to_get = 'https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=' + \
-    #     code
-    # user_json = requests.get(url_to_get)
-    # logging.info(user_json)
-    # name = user_json['name'] + ' ' + user_json['family_name']
-    cred_string = str(credentials)
-    logging.info(cred_string)
-    # creds_to_insert = {name: credentials}
-    # logging.info(creds_to_insert)
-    db.mvp.insert(cred_string)
-
-    # try:
-    #     returned_code = request.args.get('code')
-    #     # Sample to build
-    #     # POST /o/oauth2/token HTTP/1.1
-    #     # Host: accounts.google.com
-    #     # Content-Type: application/x-www-form-urlencoded
-    #     # code=4/ux5gNj-_mIu4DOD_gNZdjX9EtOFf&
-    #     # client_id=1084945748469-eg34imk572gdhu83gj5p0an9fut6urp5.apps.googleusercontent.com&
-    #     # client_secret=hDBmMRhz7eJRsM9Z2q1oFBSe&
-    #     # redirect_uri=http://localhost/oauth2callback&
-    #     # grant_type=authorization_code
-    #     url_json = {
-    #         'code': returned_code,
-    #         'client_id': '1067255681104-7dltm9n7mvb5v5ghl86p7bh1lc71jo6u.apps.googleusercontent.com',
-    #         'client_secret': 'TJit9VO6nzvJ03CRgoo3t_4e',
-    #         'redirect_uri': 'http%3A%2F%2Fli507-39.members.linode.com%2Fapi%2Frequst_fallback',
-    #         'grant_type': 'authorization_code'
-    #     }
-    #     flow = OAuth2WebServerFlow(client_id='1067255681104-7dltm9n7mvb5v5ghl86p7bh1lc71jo6u.apps.googleusercontent.com',
-    #                                client_secret='TJit9VO6nzvJ03CRgoo3t_4e',
-    #                                scope='https://www.googleapis.com/auth/youtube',
-    #                                redirect_uri='http%3A%2F%2Fli507-39.members.linode.com%2Fapi%2Frequst_fallback')
-
-    #     # Make the request to Google and hopefully get back the legit
-    #     # credentials.
-    #     response = requests.post('https://accounts.google.com/o/oauth2/token',
-    #                              data=url_json, headers={'Content-Type': 'application/json'})
-    #     logging.info("Here is the Google response")
-    #     logging.info(response)
-    #     db.mvp.insert(response)
-    #     logging.info(response)
-    # # If we got here it means that the user did not grant us access to their
-    # # YouTube account.
-    # except:
-    #     logging.info("Error: ", sys.exc_info()[0])
-    #     status_info = sys.exc_info()[0]
-    #     raise
+    return credentials
 
 
 @app.route('/api/get_subscriptions', methods=['POST'])
 def get_subscriptions():
-    user_info = request.get_json(force=True)
-    # Go to the DB and get the user.
-    user = db.mvp.find_one(user_info)
 
     # -------------------------------------
     # Request Subscription data from Google.
     # -------------------------------------
-    print(user)
 
     # Sample URL
     # curl
     # https://www.googleapis.com/youtube/v3/channels?part=id&mine=true&access_token=ACCESS_TOKEN
     fetch_url = 'https://www.googleapis.com/youtube/v3/subscriptions?mine=true&access_token=' + \
         str(user[access_token])
-    req = urllib.request(fetch_url)
+    http = credentials.authorize(http)
+    content = http.request(fetch_url, "GET")
 
-    # Sample Header
-    # Authorization: Bearer ACCESS_TOKEN
-    req.add_header('Authorization: Bearer', user[access_token])
-    resp = urllib.urlopen(req)
+    # # Sample Header
+    # # Authorization: Bearer ACCESS_TOKEN
+    # req.add_header('Authorization: Bearer', user[access_token])
+    # resp = urllib.urlopen(req)
 
     # This is a user's subscriptions
-    content = resp.read()
+    # content = resp.read()
     ids = [object["id"] for object in content["items"]]
     channels = np.array([object["channelId"] for object in [object["resourceId"]
                                                             for object in [object["snippet"] for object in content["items"]]]]).flatten()
